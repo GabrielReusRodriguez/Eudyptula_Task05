fc3d81110b930998d6dd2b3e62fac67966baf2d7
Gabriel Reus Rodriguez <gabrielin@gmail.com>
2017-07-24 14:27:33 +0200

    Version 1 funcional para un Pendrive ( cambiar la idVendor y idProduct para que se adapte al usb enchufado)

--- task5_module.c
+++ task5_module.c
@@ -10,70 +10,50 @@
 
 /* https:/github.com/martinezjavier/ldd3/bob/master/usb/usb-skeleton.c */
 
-
+/*URL:
+ * http://opensourceforu.com/2011/10/usb-drivers-in-linux-1/
+ * Si pruebas con un  pendrive usb,
+ * hay que "descargar de memoria"
+ * el modulo de usbstorage:
+ * sudo rmmod usb_storage
+ */
 
 /* Define the values to match your devices */
-#define USB_VENDOR_ID 	0xfff0
-#define USB_PRODUCT_ID 	0xfff0
+#define USB_VENDOR_ID 0x090C
+#define USB_PRODUCT_ID 0x1000
 
-static struct usb_device_id usb_table [] = {
+static struct usb_device_id usb_table[] = {
 	{
-		USB_DEVICE(USB_VENDOR_ID,USB_PRODUCT_ID)
-	},{ }
+		USB_DEVICE(USB_VENDOR_ID, USB_PRODUCT_ID)
+	}, { }
 };
 
 /* table of devices that work with this driver */
 
-MODULE_DEVICE_TABLE(usb,usb_table);
-
-/* USB Operations */
-
-static ssize_t usb_write(struct file *file, const char __user *user_buffer, size_t count, loff_t *ppos)
-{
-	int retVal = 0;
-	return retVal;
-}
-
-static ssize_t usb_read(struct file *file, char __user *buffer, size_t count, loff_t *ppos)
-{
-	int retVal = 0;
-	return retVal;
-}
-
-
-static int usb_open(struct inode *inode, struct file *file)
-{
-	int retVal = 0;
-	return retVal;
-}
-
-static int usb_release(struct inode *inode, struct file *file)
-{
-	return 0;
-}
-
-static struct file_operations usb_fops = {
-	.owner 		= THIS_MODULE,
-	.read		= usb_read,
-	.write		= usb_write,
-	.open		= usb_open,
-	.release	= usb_release
-};
+MODULE_DEVICE_TABLE(usb, usb_table);
 
 static void usb_disconnect(struct usb_interface *interface)
 {
+
+//	printk(KERN_INFO "USB Disconnected id Eudyptula: 7049f76c2121.\n");
+	pr_info("USB Disconnected id Eudyptula: 7049f76c2121.\n");
 
 
 }
 
 /* Pregunta hasta que descubre el dispositivo USB.*/
-static int usb_probe(struct usb_interface *interface, const struct usb_device_id *id)
+static int usb_probe(struct usb_interface *interface,
+		     const struct usb_device_id *id)
 {
 	int retVal = 0;
 
+	pr_info("USB ( %04X:%04X ) plugged with id Eudyptula: 7049f76c2121\n",
+		id->idVendor, id->idProduct);
 	return retVal;
 
 }
+
+
 /* USB Driver definition */
 
 static struct usb_driver my_usb_driver = {
@@ -81,7 +61,6 @@ static struct usb_driver my_usb_driver = {
 	.id_table	= usb_table,
 	.probe		= usb_probe,
 	.disconnect	= usb_disconnect
-
 };
 
 
@@ -91,20 +70,21 @@ static struct usb_driver my_usb_driver = {
 static int __init usb_inicio(void)
 {
 	int retVal = 0;
+
 	retVal = usb_register(&my_usb_driver);
-	if(retVal)
+	if (retVal)
 		//pr_err(KERN_ERR "usb_register failed.Error number %d",retVal);
-		printk(KERN_ERR "usb_register failed.Error number %d",retVal);
+		pr_info("usb_register failed.Error number %d", retVal);
 	else
-		printk(KERN_INFO "Hello World my id is: 7049f76c2121.\n");
+		pr_info("Hello World my id is: 7049f76c2121.\n");
 	return retVal;
 }
 
 static void __exit usb_fin(void)
 {
+	pr_info("USB Deregistered id Eudyptula task 05 is: 7049f76c2121.\n");
 	usb_deregister(&my_usb_driver);
-	printk(KERN_INFO "USB Deregistered id is: 7049f76c2121.\n");
-	
+
 }
 
 module_init(usb_inicio);
